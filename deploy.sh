029d9cb724934e954e2713c5ed7e8378239363af
romain <monfollet.romain@hotmail.fr>
2021-11-01 12:44:24 +0100

    fix deployment by pushing to master

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:romdarom/romdarom.github.io.git main
+git push -f git@github.com:romdarom/romdarom.github.io.git master
 
 cd -
